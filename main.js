c8c241d9a085b595c40c114fb821ce77a202d576
Geshina <geshovskileo@gmail.com>
2023-10-31 16:53:23 +0100

    gsap trial

---
+++ main.js
@@ -0,0 +1,44 @@
+// Imports
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
+
+// Lenis
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+// split-type
+const SplitTypes = document.querySelectorAll(".text-reveal");
+
+SplitTypes.forEach((char, i) => {
+  const text = new SplitType(char, {
+    types: "chars,words",
+  });
+
+  // GSAP
+  gsap.from(text.chars, {
+    scrollTrigger: {
+      trigger: char,
+      start: "-100% center",
+      end: "100% center ",
+      scrub: true,
+      markers: true,
+    },
+    opacity: 0.2,
+    stagger: 0.1,
+  });
+});
